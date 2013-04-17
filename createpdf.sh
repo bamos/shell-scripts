5cbb761365f5ee6c6f7a65fe11d1a59b713d405b
Brandon Amos <bdamos@vt.edu>
2013-04-16 21:25:08 -0400

    Add createpdf.sh.

---
+++ createpdf.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# createpdf.sh
+# Creates a printable pdf document from a plaintext file.
+#
+# Brandon Amos <http://bamos.github.io>
+# 2013.04.16
+
+function die { echo $1; exit 42; }
+
+[[ $# != 1 ]] && die "Usage: $0 <plaintext file>"
+
+TEXT_FILE=$1
+[[ -a $TEXT_FILE ]] || die "$TEXT_FILE is not a file."
+
+vim -e -s $TEXT_FILE <<EOF
+set printoptions=number:y,paper:letter
+set printheader=%<%f%h%m%=Page\ %N\ of\ %{line('$')/69+1}
+hardcopy > $TEXT_FILE.ps
+EOF
+ps2pdf14 $TEXT_FILE.ps $TEXT_FILE.pdf
+[[ $? != 0 ]] && echo "Unable to create pdf."
+
+rm -f $TEXT_FILE.ps

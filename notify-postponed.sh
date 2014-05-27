858f942c1450c9975d9f66aeaa78f2f99d43f62c
Brandon Amos <bamos@cmu.edu>
2014-05-27 01:05:49 -0700

    notify-postponed: Add case for OSX.

--- notify-postponed.sh
+++ notify-postponed.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 if [ -s $HOME/.mutt/postponed ]; then
-  notify-send 'mutt' 'Postponed messages\!'
+  case $(uname) in
+    "Linux")
+      notify-send 'mutt' 'Postponed messages\!'
+    ;;
+    "Darwin")
+      /usr/local/bin/terminal-notifier \
+        -title 'mutt' -message 'Postponed messages!'
+    ;;
+  esac
 fi
